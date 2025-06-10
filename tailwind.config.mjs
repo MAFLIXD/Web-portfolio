eeb8b5fc372489f618976909c483865b239ffceb
Santiago Mafla <santiago.mafla8@gmail.com>
2025-06-09 20:07:57 -0500

    Five commit min 30:50

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,17 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'background-shine': 'background-shine 2s linear infinite',
+      },
+      keyframes: {
+        'background-shine': {
+          from: { 'background-position': '0 0' },
+          to: { 'background-position': '-200% 0' },
+        },
+      },
+    },
   },
   plugins: [],
 }
